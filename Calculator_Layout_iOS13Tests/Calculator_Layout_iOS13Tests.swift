9f987458db10d7ef119655a383995d0b4533f7b4
rajsalla <40599929+rajsalla@users.noreply.github.com>
2025-05-25 14:28:53 +0530

    Unit tests

---
+++ Calculator_Layout_iOS13Tests/Calculator_Layout_iOS13Tests.swift
@@ -0,0 +1,20 @@
+//
+//  Calculator_Layout_iOS13Tests.swift
+//  Calculator_Layout_iOS13Tests
+//
+//  Created by Raj Salla on 2025-05-25.
+//  Copyright © 2025 The App Brewery. All rights reserved.
+//
+
+import Testing
+
+import XCTest
+@testable import Calculator_Layout_iOS13
+
+final class Calculator_Layout_iOS13Tests: XCTestCase {
+
+    func testExampleAddition() {
+        let result = 2 + 2
+        XCTAssertEqual(result, 4, "Basic math should pass")
+    }
+}
